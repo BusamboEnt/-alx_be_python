c3a736a814b447c0eb163e5c3a7c8d1212fc47c7
BusamboEnt <79867433+BusamboEnt@users.noreply.github.com>
2024-12-18 14:21:26 +0200

    basic2

--- basic_operations.py
+++ basic_operations.py
@@ -6,5 +6,5 @@ differance = number1 - number2
 product = number1 * number2
 
 print(f"Addition of {number1} and {number2} is {sum}")
-print(f"Subtration of {number1} and {number2} is {subtration}")
+print(f"Subtration of {number1} and {number2} is {differance}")
 print(f"Multiplication of {number1} and {number2} is {product}")
