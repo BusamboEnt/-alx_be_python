309b4eb6d48c47e21cc820ba51246e6a480bd1f8
BusamboEnt <79867433+BusamboEnt@users.noreply.github.com>
2024-12-18 14:27:51 +0200

    Update basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -1,2 +1,10 @@
 number1 = 10
 number2 = 5 
+
+sum = number1 + number2
+differance = number1 - number2
+product = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtration of {number1} and {number2} is {differance}")
+print(f"Multiplication of {number1} and {number2} is {product}")
